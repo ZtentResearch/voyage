50498534b92f83f528af99b55a544e7ce27f65f0
Znyd <mailboxofnabil@gmail.com>
2026-08-29 04:25:30 +0600

    cartpole with cross entropy RL

---
+++ RL/training-code/cartpole-cross-entropy.py
@@ -0,0 +1,125 @@
+import typing as tt
+from dataclasses import dataclass
+
+import gymnasium as gym
+import numpy as np
+import torch
+from torch import nn, optim
+from torch.utils.tensorboard.writer import SummaryWriter
+
+HIDDEN_SIZE = 128
+BATCH_SIZE = 16
+PERCENTILE = 70
+
+
+class Net(nn.Module):
+    def __init__(self, obs_size: int, hidden_size: int, n_actions: int):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions),
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.net(x)
+
+
+@dataclass
+class EpisodeStep:
+    observation: np.ndarray
+    action: int
+
+
+@dataclass
+class Episode:
+    reward: float
+    steps: list[EpisodeStep]
+
+
+def iterate_batches(
+    env: gym.Env, net: Net, batch_size: int
+) -> tt.Generator[list[Episode], None, None]:
+    batch = []
+    episode_reward = 0.0
+    episode_steps = []
+    obs, _ = env.reset()
+    sm = nn.Softmax(dim=1)
+    device = next(net.parameters()).device
+    while True:
+        obs_v = torch.tensor(obs, dtype=torch.float32, device=device)
+        with torch.no_grad():
+            act_probs_v = sm(net(obs_v.unsqueeze(0)))
+        act_probs = act_probs_v.cpu().numpy()[0]
+        action = np.random.choice(len(act_probs), p=act_probs)
+        next_obs, reward, is_done, is_trunc, _ = env.step(action)
+        episode_reward += float(reward)
+        step = EpisodeStep(observation=obs, action=action)
+        episode_steps.append(step)
+        if is_done or is_trunc:
+            e = Episode(reward=episode_reward, steps=episode_steps)
+            batch.append(e)
+            episode_reward = 0.0
+            episode_steps = []
+            next_obs, _ = env.reset()
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        obs = next_obs
+
+
+def filter_batch(
+    batch: list[Episode], percentile: float
+) -> tuple[torch.FloatTensor, torch.LongTensor, float, float]:
+    rewards = list(map(lambda s: s.reward, batch))
+    reward_bound = float(np.percentile(rewards, percentile))
+    reward_mean = float(np.mean(rewards))
+
+    train_obs: list[np.ndarray] = []
+    train_act: list[int] = []
+    for episode in batch:
+        if episode.reward < reward_bound:
+            continue
+        train_obs.extend(map(lambda step: step.observation, episode.steps))
+        train_act.extend(map(lambda step: step.action, episode.steps))
+
+    train_obs_v = torch.FloatTensor(np.vstack(train_obs))
+    train_act_v = torch.LongTensor(train_act)
+    return train_obs_v, train_act_v, reward_bound, reward_mean
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v1", render_mode="human")
+    assert env.observation_space.shape is not None
+    obs_size = env.observation_space.shape[0]
+    assert isinstance(env.action_space, gym.spaces.Discrete)
+    n_actions = int(env.action_space.n)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = Net(obs_size, HIDDEN_SIZE, n_actions).to(device)
+    print(f"Using device: {device}")
+    print(net)
+    objective = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(params=net.parameters(), lr=0.01)
+    writer = SummaryWriter(comment="-cartpole")
+
+    for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+        obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
+        obs_v = obs_v.to(device)
+        acts_v = acts_v.to(device)
+        optimizer.zero_grad()
+        action_scores_v = net(obs_v)
+        loss_v = objective(action_scores_v, acts_v)
+        loss_v.backward()
+        optimizer.step()
+        print(
+            "%d: loss=%.3f, reward_mean=%.1f, rw_bound=%.1f"
+            % (iter_no, loss_v.item(), reward_m, reward_b)
+        )
+        writer.add_scalar("loss", loss_v.item(), iter_no)
+        writer.add_scalar("reward_bound", reward_b, iter_no)
+        writer.add_scalar("reward_mean", reward_m, iter_no)
+        if reward_m > 475:
+            print("Solved!")
+            break
+    writer.close()
